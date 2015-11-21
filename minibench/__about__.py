a1ac66dc075181c62bb3c0d3a26beb5c46d5f4ab
Axel Haustant <noirbizarre@gmail.com>
2015-11-21 12:50:42 +0100

    Update to version 0.1.3.dev for next development cycle

--- minibench/__about__.py
+++ minibench/__about__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-__version__ = '0.1.2'
+__version__ = '0.1.3.dev'
 __description__ = 'A really simple benchmark tool'
 __author__ = 'Axel Haustant'
 __email__ = 'noirbizarre+minibench@gmail.com'
